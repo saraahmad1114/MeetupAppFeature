f96bce8dceba81e5a5385b72cecdc686e47de7de
Sara <saraahmad1114@yahoo.com>
2017-01-08 18:53:29 -0500

    json parsing

--- MeetupFeature/MeetupFeature/MeetupDataStore.swift
+++ MeetupFeature/MeetupFeature/MeetupDataStore.swift
@@ -15,8 +15,27 @@ class MeetupDataStore{
     
     var meetupArray: [SingleMeetup] = []
     
+    func getMeetupInformationWith(Completion: @escaping(Array<Any>) -> ()){
     
+        MeetupAPIClient.getMeetupInformation { (meetupArray) in
+            for singleDictionary in meetupArray{
 
+                guard let unwrappedSingleDictionary = singleDictionary as? [String: Any] else {
+                    print("singleDictionary in array did not unwrap"); return}
+                
+                
+                
+                
+                
+                
+            
+            }
+        }
+    
+    }
+    
+    
+    
 
 
 }
