f0d929eeab3c4cee26834d5e76b657ac3c083a38
Sara <saraahmad1114@yahoo.com>
2017-01-08 23:48:06 -0500

    tableview not loading

--- MeetupFeature/MeetupTableViewController.swift
+++ MeetupFeature/MeetupTableViewController.swift
@@ -14,7 +14,6 @@ class MeetupTableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         self.store.getMeetupInformationWith { (meetupObjectsArray) in
             print("**********************************")
             print(meetupObjectsArray)
